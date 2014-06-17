0083c72ccaf95ca831c0f8d7e87311b74d37d321
Steven Locke <LuckySteves@gmail.com>
2014-06-16 23:46:51 -0400

    Fixed the printing of AJQK's again
    It was deleted for some reason...

--- Table.cpp
+++ Table.cpp
@@ -22,22 +22,22 @@ void Table::playCard(Card e)
 {
     if(e.getSuit() == SPADE)
     {
-        assert(spades_[e.getRank()] == false); 
+        assert(spades_[e.getRank()] == false);
         spades_[e.getRank()] = true;
     }
     if(e.getSuit() == HEART)
     {
-        assert(hearts_[e.getRank()] == false); 
+        assert(hearts_[e.getRank()] == false);
         spades_[e.getRank()] = true;
     }
     if(e.getSuit() == CLUB)
     {
-        assert(clubs_[e.getRank()] == false); 
+        assert(clubs_[e.getRank()] == false);
         clubs_[e.getRank()] = true;
     }
     if(e.getSuit() == DIAMOND)
     {
-        assert(diamonds_[e.getRank()] == false); 
+        assert(diamonds_[e.getRank()] == false);
         diamonds_[e.getRank()] = true;
     }
 
@@ -49,15 +49,15 @@ set<Card> Table::legalMoves()
     if(spades_[SEVEN] == false)
     {
         ret.insert(Card(SPADE, SEVEN));
-        return ret; 
+        return ret;
     }
 
     for(int i = 0; i < static_cast<int>(SUIT_COUNT); i++)
     {
-        ret.insert(Card(static_cast<Suit>(i), SEVEN)); 
+        ret.insert(Card(static_cast<Suit>(i), SEVEN));
     }
 
-    //We start at 1 because we don't care about -1
+    //We start at 1 because we don't care about 0
     for(int i = 1; i < static_cast<int>(RANK_COUNT - 1); i++)
     {
         if(spades_[i] == true)
@@ -119,32 +119,34 @@ set<Card> Table::legalMoves()
 
 std::ostream &operator<<(std::ostream &sout, const Table &t)
 {
+string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+
     sout << "Cards on the table:" << endl;
     sout << "Clubs:";
     for(int i = 0; i < 13; i++)
     {
         if(t.clubs_[i])
-            sout << " " << i + 1;
+            sout << " " << ranks[i];
     }
     sout << endl << "Diamonds:";
     for(int i = 0; i < 13; i++)
     {
         if(t.diamonds_[i])
-            sout << " " << i + 1;
+            sout << " " << ranks[i];
     }
     sout << endl << "Hearts:";
     for(int i = 0; i < 13; i++)
     {
         if(t.hearts_[i])
-            sout << " " << i + 1;
+            sout << " " << ranks[i];
     }
     sout << endl << "Spades:";
     for(int i = 0; i < 13; i++)
     {
         if(t.spades_[i])
-            sout << " " << i + 1;
+            sout << " " << ranks[i];
     }
 
     return sout;
 }
-
